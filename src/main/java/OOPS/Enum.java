1ca650acc389f95f3a6bee2adaf7c71a03e03fca
megha <stutimegha2019@gmail.com>
2023-04-20 16:28:55 +0530

    enum

---
+++ src/main/java/OOPS/Enum.java
@@ -0,0 +1,11 @@
+package OOPS;
+
+public class Enum {
+    public  enum Seasons{WINTER, SPRING, SUMMER}
+
+    public static void main(String args[]){
+        for (Seasons s : Seasons.values()){
+            System.out.println(s);
+        }
+    }
+}
